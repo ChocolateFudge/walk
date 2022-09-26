8b44f012676774b210061544d215630803f477ae
choco <harsh.kis@student.mahidol.edu>
2022-09-26 23:10:41 +0700

    First proper intelij file working on Debian 22.04 With no maijor problems

--- src/main/java/io/muzoo/ssc/Main.java
+++ src/main/java/io/muzoo/ssc/Main.java
@@ -1,7 +1,23 @@
 package io.muzoo.ssc;
 
+import org.apache.commons.cli.*;
+import org.codehaus.plexus.util.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Options options = new Options();
+        options.addOption("a", "--total-num-files", true, "Print the total number of files");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine c = null;
+        try {
+            c = parser.parse(options, args);
+            if (c.hasOption("a"))
+                System.out.println("This is working");
+        } catch (ParseException e) {
+            throw new RuntimeException("FAILED");
+        }
+
+
     }
+
 }
